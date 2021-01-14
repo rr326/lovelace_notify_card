e973cd11816c7ed48217bfab8b2618fd64de3a93
Ross Rosen <rrosen326@gmail.com>
2021-01-14 14:14:29 -0800

    working interim

--- src/notify-card.js
+++ src/notify-card.js
@@ -15,8 +15,12 @@ export class NotifyCard extends LitElement {
     return {
       hass: {},
       config: {},
-      count: { type: Number },
+      count: { type: Number, hasChanged: () => false },
     };
+  }
+  
+  get_notify_state(obj) {
+    return obj && obj.states && obj.states["ll_notify.state"]
   }
 
   constructor() {
@@ -42,11 +46,23 @@ export class NotifyCard extends LitElement {
     `;
   }
 
+  shouldUpdate(changedProps) {
+    if (changedProps.get("hass")) {
+      let oldval = this.get_notify_state(changedProps.get("hass"))
+      let newval = this.get_notify_state(this.hass)
+      if (newval !== oldval) {
+        return true
+      }
+    } 
+    console.log('shouldUpdate - false', changedProps)
+    return false
+  }
+
   render() {
     window._ross = this;
-    let ll_notify = this.hass.states['ll_notify.state'],
-      attributes = ll_notify ? ll_notify.attributes : null,
-      att_ar = attributes ? Object.entries(attributes) : null;
+    let ll_notify = this.get_notify_state(this.hass),
+      attributes = ll_notify && ll_notify.attributes,
+      att_ar = attributes && Object.entries(attributes);
     // console.log('hass', this.hass);
     // console.log(`attributes`, ll_notify, att_ar);
     ++this.count;
