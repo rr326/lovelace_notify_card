c38dac1fe936fc9df75b1c34c4c00606e50b6708
Ross Rosen <rrosen326@gmail.com>
2021-01-14 10:26:42 -0800

    interim- working

--- src/notify-card.js
+++ src/notify-card.js
@@ -11,34 +11,6 @@ import {
 } from 'lit-element';
 
 export class NotifyCard extends LitElement {
-
-  shouldUpdate(changedProps) {
-    return true // TODO Fix 
-
-    if (!this.config) {
-      return false;
-    }
-
-    return hasConfigOrEntityChanged(this, changedProps, false);
-  }  
-
-  render() {
-    return html`
-      <h1>NUMBER: 8</h1>
-      <h1>Lovelace Notify Card</h1>
-      <h2>Other stuff here</h2>
-      <p>Lorem ipsum...</p>
-    `;
-  }
-
-  static get styles() {
-    return css`
-    :host {
-      background-color: red;
-    }
-    `
-  }
-
   setConfig(config) {
     //   if (!config.entity) {
     //     throw new Error('You need to define an entity');
@@ -46,10 +18,35 @@ export class NotifyCard extends LitElement {
     this.config = config;
   }
 
-  // The height of your card. Home Assistant uses this to automatically
-  // distribute all cards over the available columns.
   getCardSize() {
     return 3;
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        background-color: red;
+      }
+    `;
+  }
+
+  render() {
+    return html`
+      <h1>NUMBER: 9</h1>
+      <h1>Lovelace Notify Card</h1>
+      <h2>Other stuff here</h2>
+      <p>Lorem ipsum...</p>
+    `;
+  }
+
+  shouldUpdate(changedProps) {
+    return true; // TODO Fix
+
+    if (!this.config) {
+      return false;
+    }
+
+    return hasConfigOrEntityChanged(this, changedProps, false);
   }
 }
 customElements.define('notify-card', NotifyCard);
