9da0412db65840b81f31f70c4b5d1fe908b5d6d6
Ross Rosen <rrosen326@gmail.com>
2021-01-14 13:35:52 -0800

    interim -working

--- src/notify-card.js
+++ src/notify-card.js
@@ -11,10 +11,17 @@ import {
 } from 'lit-element';
 
 export class NotifyCard extends LitElement {
+  static get properties() {
+    return {
+      hass: {},
+      config: {},
+      count: { type: Number },
+    };
+  }
+
   constructor() {
-    super()
-    window._ross = this
-    console.log('hass', this.hass)
+    super();
+    this.count = 0;
   }
   setConfig(config) {
     //   if (!config.entity) {
@@ -36,25 +43,39 @@ export class NotifyCard extends LitElement {
   }
 
   render() {
-    window._ross = this
-    console.log('hass', this.hass)
+    window._ross = this;
+    let ll_notify = this.hass.states['ll_notify.state'],
+      attributes = ll_notify ? ll_notify.attributes : null,
+      att_ar = attributes ? Object.entries(attributes) : null;
+    // console.log('hass', this.hass);
+    // console.log(`attributes`, ll_notify, att_ar);
+    ++this.count;
+    console.log(`${this.count} - Rendered`);
 
     return html`
       <h1>NUMBER: 9</h1>
       <h1>Lovelace Notify Card</h1>
-      <h2>hass</h2>
-      <p>${this.hass}</p>
+      <h2>Current count: ${this.count}</h2>
+      <ul>
+        ${att_ar.map(
+          item =>
+            html`
+              <li>${item}</li>
+            `,
+        )}
+      </ul>
     `;
   }
 
-  shouldUpdate(changedProps) {
-    return true; // TODO Fix
+  // shouldUpdate(changedProps) {
+  //   console.log(`shouldUpdate: `, changedProps)
+  //   return true; // TODO Fix
 
-    if (!this.config) {
-      return false;
-    }
+  //   if (!this.config) {
+  //     return false;
+  //   }
 
-    return hasConfigOrEntityChanged(this, changedProps, false);
-  }
+  //   return hasConfigOrEntityChanged(this, changedProps, false);
+  // }
 }
 customElements.define('notify-card', NotifyCard);
